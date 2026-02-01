7d4a23f700e30ecad4d239def6d13f76c32e3f1d
mmvES <miguerepvfr@gmail.com>
2026-02-01 16:36:09 +0100

    Refactor AuthService to use environment variables for BASE_URL and streamline login response handling

--- desktop_app/repo/auth_service.py
+++ desktop_app/repo/auth_service.py
@@ -1,39 +1,39 @@
 import requests
-from src.desktop_app.config import Config
+from dotenv import load_dotenv
+import os
+class AuthService():
 
-class AuthService:
-    # Variables de clase para mantener la sesión (Singleton)
-    _token = None
-    _user = None
-
+    load_dotenv()
+    
+    BASE_URL = f"http://{os.getenv("DB_HOST")}:{os.getenv("API_PORT")}"
+    
     def login(self, email, password, rol):
+        endpoint = f"{self.BASE_URL}/login"
+        payload = {
+            "email": email,
+            "password": password,
+            "rol": rol
+        }
+        
         try:
-            url = f"{Config.API_URL}/login"
-            payload = {"email": email, "password": password, "rol": rol}
-            
-            response = requests.post(url, json=payload)
+            response = requests.post(endpoint, json=payload)
             
             if response.status_code == 200:
-                data = response.json()
-                # GUARDAMOS LA SESIÓN AQUÍ AUTOMÁTICAMENTE
-                AuthService.set_session(data.get("access_token"), data.get("user"))
-                return {"success": True, "data": data}
+                data=response.json()
+                
+                
+                return {
+                    "success": True,
+                    "user": data.get("user"),
+                    "access_token": data.get("access_token")
+                    
+                }
             else:
-                return {"success": False, "msg": response.json().get("msg", "Error desconocido")}
+                return {
+                    "Code": response.status_code,
+                    "Success": False,
+                    "error": response.json().get("msg")
+                }
         except Exception as e:
-            return {"success": False, "msg": f"Error de conexión: {str(e)}"}
-
-    # --- MÉTODOS DE GESTIÓN DE SESIÓN ---
-    @classmethod
-    def set_session(cls, token, user):
-        cls._token = token
-        cls._user = user
-
-    @classmethod
-    def get_token(cls):
-        """Recupera el token actual"""
-        return cls._token
-
-    @classmethod
-    def get_user(cls):
-        return cls._user
+            return {"success": False, "error": f"Error de conexión: {str(e)}"}
+            
